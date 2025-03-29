e9987d60ace1c4932ec3b58cddd0538a5fdd46ca
Andrew Sabelhaus <asabelha@bu.edu>
2025-03-29 09:14:05 -0400

    subscriber draft, some cleanup and debugging messages

---
+++ ezloophwROS2BendingAngleCallback.m
@@ -0,0 +1,27 @@
+function ezloophwROS2BendingAngleCallback(message)
+    %exampleHelperROS2PoseCallback Subscriber callback function for pose data    
+    %   exampleHelperROS2PoseCallback(MESSAGE) returns no arguments - it instead sets 
+    %   global variables to the values of position and orientation that are
+    %   received in the ROS 2 message MESSAGE.
+    %   
+    %   See also ROSPublishAndSubscribeExample.
+    
+    %   Copyright 2019 The MathWorks, Inc.
+    
+    % Declare global variables to store position and orientation
+    % global pos
+    % global orient
+    
+    global bendingVec
+    
+    % Extract position and orientation from the ROS message and assign the
+    % data to the global variables.
+    % pos = [message.linear.x message.linear.y message.linear.z];
+    % orient = [message.angular.x message.angular.y message.angular.z];
+
+    if ~isempty(message.data)
+        bendingVec = bendingMsg.Data(:)';  % Row vector
+        disp('Received ', string(bendingVec));
+    end
+
+end
--- ros2_matlab_basic.m
+++ ros2_matlab_basic.m
@@ -1,7 +1,13 @@
 % Modified 2025-03-26 for ROS2. -Drew
 
-% Initialize ROS core
-% rosinit;
+%%% Setup just to make sure we start at the same state each time.
+clear all
+close all
+clc;
+
+disp('ROS2 Matlab demo for communication with Python.');
+disp('(C) Soft Robotics Control Lab at Boston University, 2025');
+disp('creating the nodes / starting ros2...');
 
 %%% Initialize ROS2 nodes for the MATLAB side
 % we are this node for sending control commands
@@ -15,15 +21,32 @@ matlabSubNode = ros2node(matlabSubNodeName);
 % controlPub = rospublisher('/control_values', 'std_msgs/Float64MultiArray');
 % controlMsg = rosmessage(controlPub);
 
+pubWait = 5;
+
+disp("Starting the publisher, waiting " + string(pubWait) + " seconds...");
+
 controlPub = ros2publisher(matlabPubNode, '/control_values', 'std_msgs/Float64MultiArray');
 controlMsg = ros2message(controlPub);
-pause(10); %wait for some time to register publisher on the network
+pause(pubWait); %wait for some time to register publisher on the network
 
 % --- Setup: Subscriber to /bending_angles ---
 % bendingSub = rossubscriber('/bending_angles', 'std_msgs/Float64MultiArray');
 % bendingSub = ros2subscriber(matlabSubNode, '/bending_angles', 'std_msgs/Float64MultiArray');
-% debugging
-bendingSub = ros2subscriber(matlabSubNode, '/control_values', 'std_msgs/Float64MultiArray');
+
+subStarted = 0;
+substartwait = 5;
+
+disp('Attempting to start the subscriber...')
+while ~subStarted
+    try
+        bendingSub = ros2subscriber(matlabSubNode, '/bending_angles', @ezloophwROS2BendingAngleCallback);
+        subStarted = 1;
+    catch
+        disp("ERROR! You need to start the bending_angles publisher in python.");
+        disp("Waiting another " + string(substartwait) + " seconds, trying again...");
+        pause(substartwait);
+    end
+end
 
 % --- Rates ---
 pubRate = 1;    % 1 Hz publish
@@ -38,61 +61,63 @@ timerHandles.controlPub = controlPub;
 timerHandles.controlPubmsg = controlMsg;
 simTimer = ezloophwROS2Timer(pubRate, {@ezloophwROS2SimTimer,timerHandles});
 
-disp('breakpoint');
+disp('breakpoint here to check the publisher only');
 
-% lastPubTime = rostime('now');
-% lastSubTime = rostime('now');
-disp('ROS active. Press "q" to stop.');
+global bendingVec;
 
-fig = figure('Name', 'Press "q" to quit');
-set(fig, 'KeyPressFcn', @keyPressCallback);
-
-% Data log: [timestamp, angle1, angle2]
-bendingLog = [];
-t = 0;
-% Global flag
-global stopFlag
-stopFlag = false;
-
-while ~stopFlag
-    % Allow figure window and callbacks to process
-    drawnow;
-    % --- Publish control_values at 1 Hz ---
-    % currentTime = rostime('now');
-    % if currentTime.Sec - lastPubTime.Sec >= 1.0/pubRate
-    %     controlMsg.Data = rand(2,1);
-    %     send(controlPub, controlMsg);
-    %     disp(['[Publish] control_values: ', mat2str(controlMsg.Data')]);
-    %     lastPubTime = currentTime;
-    % end
-    % --- Read latest from bending_angles ---
-    bendingMsg = bendingSub.LatestMessage;
-        if ~isempty(bendingMsg)
-            bendingVec = bendingMsg.Data(:)';  % Row vector
-            %t = double(bendingMsg.Header.Stamp.Sec) + double(bendingMsg.Header.Stamp.Nsec) * 1e-9;
-            if t == 0  % fallback if header timestamp is missing
-                t_zero = double(rostime('now').Sec) + double(rostime('now').Nsec) * 1e-9;
-                t = 0.001;
-            else
-                t = double(rostime('now').Sec) + double(rostime('now').Nsec) * 1e-9 - t_zero;
-            end
-            logRow = [t, bendingVec];  % [timestamp, angle1, angle2]
-            bendingLog(end+1, :) = logRow;
-            disp(['[Subscribe] bending_angles: ', mat2str(bendingVec)]);
-        end
-    % Wait to maintain loop timing
-    waitfor(loopRate);
-end
-disp('Shutting down ROS...');
-rosshutdown;
-close all;
-% Save to CSV
-writematrix(bendingLog, 'bendingLog.csv');
-disp('Logged data saved to bendingLog.csv');
-
-function keyPressCallback(~, event)
-    global stopFlag
-    if strcmp(event.Key, 'q')
-        stopFlag = true;
-    end
-end
+% % lastPubTime = rostime('now');
+% % lastSubTime = rostime('now');
+% disp('ROS active. Press "q" to stop.');
+% % 
+% fig = figure('Name', 'Press "q" to quit');
+% set(fig, 'KeyPressFcn', @keyPressCallback);
+% 
+% % Data log: [timestamp, angle1, angle2]
+% bendingLog = [];
+% t = 0;
+% % Global flag
+% global stopFlag
+% stopFlag = false;
+% 
+% while ~stopFlag
+%     % Allow figure window and callbacks to process
+%     drawnow;
+%     % --- Publish control_values at 1 Hz ---
+%     % currentTime = rostime('now');
+%     % if currentTime.Sec - lastPubTime.Sec >= 1.0/pubRate
+%     %     controlMsg.Data = rand(2,1);
+%     %     send(controlPub, controlMsg);
+%     %     disp(['[Publish] control_values: ', mat2str(controlMsg.Data')]);
+%     %     lastPubTime = currentTime;
+%     % end
+%     % --- Read latest from bending_angles ---
+%     bendingMsg = bendingSub.LatestMessage;
+%         if ~isempty(bendingMsg)
+%             bendingVec = bendingMsg.Data(:)';  % Row vector
+%             %t = double(bendingMsg.Header.Stamp.Sec) + double(bendingMsg.Header.Stamp.Nsec) * 1e-9;
+%             if t == 0  % fallback if header timestamp is missing
+%                 t_zero = double(rostime('now').Sec) + double(rostime('now').Nsec) * 1e-9;
+%                 t = 0.001;
+%             else
+%                 t = double(rostime('now').Sec) + double(rostime('now').Nsec) * 1e-9 - t_zero;
+%             end
+%             logRow = [t, bendingVec];  % [timestamp, angle1, angle2]
+%             bendingLog(end+1, :) = logRow;
+%             disp(['[Subscribe] bending_angles: ', mat2str(bendingVec)]);
+%         end
+%     % Wait to maintain loop timing
+%     waitfor(loopRate);
+% end
+% disp('Shutting down ROS...');
+% rosshutdown;
+% close all;
+% % Save to CSV
+% writematrix(bendingLog, 'bendingLog.csv');
+% disp('Logged data saved to bendingLog.csv');
+% 
+% function keyPressCallback(~, event)
+%     global stopFlag
+%     if strcmp(event.Key, 'q')
+%         stopFlag = true;
+%     end
+% end
